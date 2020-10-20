043d16461e3e9f1176a429de747715ce6fd3063c
Alex Camacho <alsbrain@optonline.net>
2020-10-20 08:02:01 -0700

    added timer

--- script.js
+++ script.js
@@ -13,19 +13,43 @@ var firstAnswer = [
     "This is choice D"
 ]
 
-var startButton = document.querySelector("#start-button")
+var timer = document.querySelector(".timer")
 var header = document.querySelector("#header")
 var intro = document.querySelector("#intro")
-
+var questionBox = document.querySelector("#question-box")
+var startButton = document.querySelector("#start-button")
+var interval
 
 // Quiz start button event listener.
 startButton.addEventListener('click', function(event){
     event.preventDefault()
-    console.log(startButton)
     header.style.display = "none"
     startButton.style.display = "none"
     intro.style.display = "none"
     // Function that wll initiate the quiz
-    // startQuiz()
+    timer.innerHTML = startTimer()
 })
 
+// Start timer
+// Keep counting down as long as timer is above 0
+function startTimer() {
+    var timer = 60
+    if (timer > 0){
+        interval = setInterval(function(){
+            timer--
+            return renderTimer(timer)
+        }, 1000)
+    } else {
+        //When time reaches 0 end game
+        // endGame()
+    }
+}
+
+function renderTimer(num) {
+    timer.innerHTML = num
+    console.log(timer.innerHTML)
+}
+
+function renderQuestions(){
+
+}
