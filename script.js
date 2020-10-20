527c7b0bccd3e14cb5f995d01172ab5116af0604
Alex Camacho <alsbrain@optonline.net>
2020-10-20 08:45:58 -0700

    added function to render question and choices

--- script.js
+++ script.js
@@ -6,21 +6,54 @@ var questions = [
     "This is the fifth question",
 ]
 // Templates to hold questions and answers
-var firstAnswer = [
-    "This is choice A",
-    "This is choice B",
-    "This is choice C",
-    "This is choice D"
+var answers = [
+{
+    "0": "This is choice A",
+    "1": "This is choice B",
+    "2": "This is choice C",
+    "3": "This is choice D"
+},
+{
+    "0": "This is choice A",
+    "1": "This is choice B",
+    "2": "This is choice C",
+    "3": "This is choice D"
+},
+{
+    "0": "This is choice A",
+    "1": "This is choice B",
+    "2": "This is choice C",
+    "3": "This is choice D"
+},
+{
+    "0": "This is choice A",
+    "1": "This is choice B",
+    "2": "This is choice C",
+    "3": "This is choice D"
+},
+{
+    "0": "This is choice A",
+    "1": "This is choice B",
+    "2": "This is choice C",
+    "3": "This is choice D"
+}
 ]
+
 
 var timer = document.querySelector(".timer")
 var header = document.querySelector("#header")
 var intro = document.querySelector("#intro")
 var questionBox = document.querySelector("#question-box")
+var questionCounter = 0
+var answerBox = document.querySelector("#answer-box")
+var choices
 var startButton = document.querySelector("#start-button")
 var interval
 
 // Quiz start button event listener.
+// When start is clicked
+// Start timer
+// Display question with choices
 startButton.addEventListener('click', function(event){
     event.preventDefault()
     header.style.display = "none"
@@ -28,6 +61,8 @@ startButton.addEventListener('click', function(event){
     intro.style.display = "none"
     // Function that wll initiate the quiz
     timer.innerHTML = startTimer()
+    renderQuestions()
+    renderChoices()
 })
 
 // Start timer
@@ -51,5 +86,22 @@ function renderTimer(num) {
 }
 
 function renderQuestions(){
+    if(questionCounter < questions.length)
+    questionBox.innerHTML = questions[questionCounter]
+    for (var i = 0; i < 4; i++){
+    var item = document.createElement("button")
+    item.setAttribute('class', 'btn-block')
+    item.textContent = answers[questionCounter][i]
+    answerBox.appendChild(item)
+    
+    }}
 
+
+function renderChoices(){
+    choices = "<ul>"
+    answers.forEach(myFunction)
+    choices += "</ul>"
 }
+function myFunction(value) {
+    choices += "<li>" + value + "</li>";
+  } 
