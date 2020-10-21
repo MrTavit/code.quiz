e001a5d1fc183ae4baa8984c56b527820d4eda64
Alex Camacho <alsbrain@optonline.net>
2020-10-20 18:23:59 -0700

    formatting

--- script.js
+++ script.js
@@ -7,36 +7,36 @@ var questions = [
 ]
 // Templates to hold questions and answers
 var answers = [
-{
-    "0": "This is choice A",
-    "1": "This is choice B",
-    "2": "This is choice C",
-    "3": "This is choice D"
-},
-{
-    "0": "This is question 2",
-    "1": "This is choice B",
-    "2": "This is choice C",
-    "3": "This is choice D"
-},
-{
-    "0": "This is question 3",
-    "1": "This is choice B",
-    "2": "This is choice C",
-    "3": "This is choice D"
-},
-{
-    "0": "This is question 4",
-    "1": "This is choice B",
-    "2": "This is choice C",
-    "3": "This is choice D"
-},
-{
-    "0": "This is question 5",
-    "1": "This is choice B",
-    "2": "This is choice C",
-    "3": "This is choice D"
-}
+    {
+        "0": "This is choice A",
+        "1": "This is choice B",
+        "2": "This is choice C",
+        "3": "This is choice D"
+    },
+    {
+        "0": "This is question 2",
+        "1": "This is choice B",
+        "2": "This is choice C",
+        "3": "This is choice D"
+    },
+    {
+        "0": "This is question 3",
+        "1": "This is choice B",
+        "2": "This is choice C",
+        "3": "This is choice D"
+    },
+    {
+        "0": "This is question 4",
+        "1": "This is choice B",
+        "2": "This is choice C",
+        "3": "This is choice D"
+    },
+    {
+        "0": "This is question 5",
+        "1": "This is choice B",
+        "2": "This is choice C",
+        "3": "This is choice D"
+    }
 ]
 
 
@@ -54,7 +54,7 @@ var interval
 // When start is clicked
 // Start timer
 // Display question with choices
-startButton.addEventListener('click', function(event){
+startButton.addEventListener('click', function (event) {
     event.preventDefault()
     header.style.display = "none"
     startButton.style.display = "none"
@@ -69,55 +69,56 @@ startButton.addEventListener('click', function(event){
 // Keep counting down as long as timer is above 0
 function startTimer() {
     var timer = 10
-    interval = setInterval(function(){
-    if (timer > 0){
+    interval = setInterval(function () {
+        if (timer > 0) {
             timer--
             // console.log(timer)
             return renderTimer(timer)
-    } else {
-        //When time reaches 0 end game
-        // endGame()
-        clearInterval(interval)
-        console.log("Time's up!")
-    }
-        }, 1000)
+        } else {
+            //When time reaches 0 end game
+            // endGame()
+            clearInterval(interval)
+            console.log("Time's up!")
+            // enterInitials() - function for entering initials when time is up
+        }
+    }, 1000)
 }
 
 function renderTimer(num) {
     timer.innerHTML = num
 }
-// change to create buttons first, then change innerHTML
 
-function renderQuestions(){
-    
+function renderQuestions() {
+
     // Resets the answerBox to an empty string
     answerBox.innerHTML = ""
-    
+
     // counter for questions asked vs total number of questions    
-    if(questionCounter < questions.length)
+    if (questionCounter < questions.length) {
 
-    // change HTML of question to question based on counter
-    questionBox.innerHTML = questions[questionCounter]
+        // change HTML of question to question based on counter
+        questionBox.innerHTML = questions[questionCounter]
 
-    // Creates buttons for answer choices
-    // hard coded for 4 choices
-    for (var i = 0; i < 4; i++){
-    var item = document.createElement("button")
-    // Set classes to buttons for formatting
-    // id is set to the index, for keeping score later
-    item.setAttribute('class', 'btn-block btn-primary')
-    item.setAttribute('id', `choice${(i + 1)} `)
-    // Text content of button is answers object, with the question counter and incrementer as arguments
-    item.textContent = answers[questionCounter][i]
-    answerBox.appendChild(item)
-    
+        // Creates buttons for answer choices
+        // hard coded for 4 choices
+        for (var i = 0; i < 4; i++) {
+            var item = document.createElement("button")
+            // Set classes to buttons for formatting
+            // id is set to the index, for keeping score later
+            item.setAttribute('class', 'btn-block btn-primary')
+            item.setAttribute('id', `choice${(i + 1)} `)
+            // Text content of button is answers object, with the question counter and incrementer as arguments
+            item.textContent = answers[questionCounter][i]
+            answerBox.appendChild(item)
+
+        }
     }
 }
-
 // Click event listener added to the answer box.
 // When one of the options in the answer box is selected, the next question will be displayed
-answerBox.addEventListener('click', function(event){
+answerBox.addEventListener('click', function (event) {
     event.preventDefault()
     questionCounter++
     renderQuestions()
 })
+
